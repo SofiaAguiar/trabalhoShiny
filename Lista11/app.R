0a0427903528b8d1614a8b56afbb2f49e48114ca
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2021-08-24 22:23:59 -0300

    adds tablerDash to project

--- Lista11/app.R
+++ Lista11/app.R
@@ -1,49 +1,37 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tablerDash)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+shiny::shinyApp(
+    ui = tablerDashPage(
+        enable_preloader = TRUE,
+        loading_duration = 4,
+        navbar = tablerDashNav(
+            id = "myopia-menu",
+            src = "https://i.dlpng.com/static/png/6521175_preview.png",
+            navMenu = tablerNavMenu(
+                tablerNavMenuItem(
+                    tabName = "BancoDados",
+                    icon = "home",
+                    "Banco de dados"
+                ),
+                tablerNavMenuItem(
+                    tabName = "Histograma",
+                    icon = "box",
+                    "Histograma"
+                ),
+                tablerNavMenuItem(
+                    tabName = "Etc",
+                    icon = "box",
+                    "Etc"
+                ),
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+        footer = tablerDashFooter(
+            copyrights = "Análise Myopia/Aplore3. @Aline Pinto & @Sofia Aguiar, 2021"
+        ),
+        title = "Myopia - Aplore3",
+        ),
+    
+    server = function(input, output, session) {}
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
